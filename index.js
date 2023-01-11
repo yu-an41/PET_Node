e25bf2ea5d134239b05f0d6c93f200683d609a51
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-01-11 23:36:17 +0800

    add cors & /member/login/api

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const db = require(__dirname + '/modules/db_connect');
+const db = require('./modules/db_connect');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 // 解析 body
 const myParser = require('body-parser');
@@ -10,10 +12,24 @@ app.use(myParser.json());
 app.use(myParser.urlencoded({ extend: false}));
 
 //白名單
+const corsOptions = {
+    credentials: true,
+    origin: function (origin, callback) {
+        console.log(origin, callback);
+        callback(null, true);
+    },
+};
 
 //靜態資料夾
 app.use(express.static('public'));
 
+app.use(async (req, res, next) => {
+    // JWT auth
+    res.locals.auth = {};
+    let auth = req.get("Authorization");
+
+    //待補上
+})
 // router
 // 首頁
 app.get('/', (req, res) => {
--- routes/member.js
+++ routes/member.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const db = require(__dirname + '/modules/db_connect');
+const db = require(__dirname + '/../modules/db_connect');
 const jwt = require('jsonwebtoken');
 
-router.post('/login/api', async (req, res)=> {
-    const sql = 'SELECT * FROM member WHERE sid = 1';
+router.post('/login/api', (req, res)=> {
+    const output = {
+        success: false,
+        error: '',
+        auth: {},
+    };
+    console.log(req.body.account);
+    res.json(req.body);
+    // const sql = 'SELECT * FROM member WHERE sid = ?';
 
+    // const [rows] = await db.query(sql, req.body.account);
+    // console.log({rows});
+    // res.json({rows})
 })
 
 
