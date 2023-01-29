40ebf14c31723aec8f9132b6a95b5eb7993f7f60
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-01-29 23:22:31 +0800

    add linePayConfirm

--- routes/cart.js
+++ routes/cart.js
@@ -17,7 +17,40 @@ const {
   LINEPAY_RETURN_HOST,
   LINEPAY_RETURN_CONFIRM_URL,
   LINEPAY_RETURN_CANCEL_URL,
+  uri,
 } = process.env;
+
+function createLinePayBody(order) {
+  return {
+    ...order,
+    currency: "TWD",
+    redirectUrls: {
+      confirmUrl: `${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CONFIRM_URL}`,
+      cancelUrl: `${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CANCEL_URL}`,
+    },
+  };
+}
+
+function createSignature(uri, linePayBody) {
+  const nonce = parseInt(new Date().getTime() / 1000);
+
+  const encrypt = HmacSHA256(
+    `${LINEPAY_CHANNEL_SECRET_KEY}/${LINEPAY_VERSION}${uri}${JSON.stringify(
+      linePayBody
+    )}${nonce}`,
+    LINEPAY_CHANNEL_SECRET_KEY
+  );
+
+  const signature = Base64.stringify(encrypt);
+
+  const headers = {
+    "Content-Type": "application/json",
+    "X-LINE-ChannelId": LINEPAY_CHANNEL_ID,
+    "X-LINE-Authorization-Nonce": nonce,
+    "X-LINE-Authorization": signature,
+  };
+  return headers;
+}
 
 router.post("/createOrder", async (req, res) => {
   const output = {
@@ -27,6 +60,7 @@ router.post("/createOrder", async (req, res) => {
   };
 
   const { order, member_sid, payWay } = req.body;
+  console.log(member_sid);
 
   const ordernum = dayjs(new Date()).format("YYYYMMDDHHmmss");
 
@@ -34,26 +68,135 @@ router.post("/createOrder", async (req, res) => {
   let total = 0;
 
   // 先撈商品金額＆資訊
-  for(let i = 0; i < order.length; i++) {
+  for (let i = 0; i < order.length; i++) {
+    // console.log(order[i]);
+    // console.log(`id: `, order[i].id);
     const data_sql = `SELECT * FROM products WHERE sid = ${order[i].id}`;
     const [result] = await db.query(data_sql);
-    order[i].originalPrice = +result[0].price;
-    order[i].price = order[i].member_price;
-    order[i].name = result[0].name;
+    order[i].member_price = result[0].member_price;
+    order[i].origin_subtotal = +result[0].price * order[i].quantity;
+    order[i].total_subtotal = result[0].member_price * order[i].quantity;
+    order[i].product_name = result[0].name;
+    // console.log(order[i].product_name);
+    // console.log(result[0].name);
 
+    // 順便加總母訂單金額
+    originTotal += order[i].origin_subtotal;
+    total += order[i].total_subtotal;
+
+    // 先寫入子訂單
+    const details_sql = `INSERT INTO order_details (order_num, product_sid, product_name, quantity, origin_subtotal, total_subtotal) VALUES (?, ?, ?, ?, ?, ?)`;
+    const [details_result] = await db.query(details_sql, [
+      ordernum,
+      order[i].id,
+      order[i].product_name,
+      order[i].quantity,
+      order[i].origin_subtotal,
+      order[i].total_subtotal,
+    ]);
   }
 
-  // 先寫入子訂單，順便加總金額
-  for (let i = 0; i < order.length; i++) {
-    const data_sql = `SELECT * FROM orders WHERE order_id = ${order[i].id}`;
-    const [result] = await db.query(data_sql);
-    order[i].price = +result[0].price;
-    order[i].name = result[0].name;
+  // 寫入母訂單
+  const history_sql = `INSERT INTO order_history (order_num, original_total, total, member_sid, payment_sid, order_status) VALUES (?,?,?,?,?,?)`;
+  const [history_result] = await db.query(history_sql, [
+    ordernum,
+    originTotal,
+    total,
+    member_sid,
+    payWay,
+    5,
+  ]);
 
-    const details_sql = `INSERT INTO order_details (order_num, product_sid, product_name, quantity, origin_price, total_price) VALUES ()`;
+  // 把東西整理成 linePay 格式
+  const uri = "/payments/request";
 
+  const linePayData = {
+    channel_id: LINEPAY_CHANNEL_ID,
+    channel_secret: LINEPAY_CHANNEL_SECRET_KEY,
+    version: LINEPAY_VERSION,
+    site: LINEPAY_SITE,
+  };
 
+  const items = order.map((i) => {
+    return {
+      name: i.product_name,
+      quantity: i.quantity,
+      price: i.member_price,
+    };
+  });
+
+  const linePayBody = {
+    orderId: ordernum,
+    amount: total,
+    currency: "TWD",
+    redirectUrls: {
+      confirmUrl: `${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CONFIRM_URL}`,
+      cancelUrl: `${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CANCEL_URL}`,
+    },
+    packages: [
+      {
+        id: "1",
+        amount: total,
+        products: items,
+      },
+    ],
+  };
+
+  const headers = createSignature(uri, linePayBody);
+  const url = `${LINEPAY_SITE}/${LINEPAY_VERSION}${uri}`;
+  const { data } = await axios.post(url, linePayBody, { headers });
+  console.log(data);
+
+  if (data.returnCode === "0000") {
+    output.success = true;
+    output.url = data.info.paymentUrl.web;
+    output.ordernum = ordernum;
+  } else {
+    output.error = "LinePay 請求失敗";
   }
+
+  res.json({ output });
 });
 
+router.get("/linePayConfirm", async (req, res) => {
+  const output = {
+    success: false,
+    error: "",
+  };
+
+  //接下來處理linepay二次請求
+  //先找出此訂單的價格塞進amount
+  const { transactionId, orderId } = req.query;
+  const sql = `SELECT * FROM order_history WHERE order_num = "${orderId}"`;
+  const [result] = await db.query(sql);
+
+  try {
+    // 建立 LINE Pay 請求規定的資料格式
+    const uri = `/payments/${transactionId}/confirm`;
+    const linePayBody = {
+      amount: result[0].total,
+      currency: "TWD",
+    };
+
+    // CreateSignature 建立加密內容
+    const headers = createSignature(uri, linePayBody);
+
+    // API 位址
+    const url = `${LINEPAY_SITE}/${LINEPAY_VERSION}${uri}`;
+    const { data } = await axios.post(url, linePayBody, { headers });
+
+    console.log(data);
+    
+    if (data.returnCode === "0000") {
+      const sql = `UPDATE order_history SET order_status = 1 WHERE order_num = "${orderId}"`;
+      const result = await db.query(sql);
+      output.success = true;
+    } else {
+      output.error = "LinePay訂單失敗";
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  res.json({ output });
+});
 module.exports = router;
--- routes/member.js
+++ routes/member.js
@@ -1,88 +1,92 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require(__dirname + '/../modules/db_connect');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const db = require(__dirname + "/../modules/db_connect");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 // 測試api
-router.get('/login/api', async (req, res)=> {
-    const sql = 'SELECT * FROM member WHERE sid = 1';
-    const [rows] = await db.query(sql);
-    console.log('ABC');
-    res.json({ABC: 1})
-})
+router.get("/login/api", async (req, res) => {
+  const sql = "SELECT * FROM member WHERE sid = 1";
+  const [rows] = await db.query(sql);
+  console.log("ABC");
+  res.json({ ABC: 1 });
+});
 
-router.post('/login/api', async (req, res)=> {
-    let output = {
-        success: false,
-        error: '',
-        member_sid: 0,
-        nickname: '',
-        token: '',
-    };
-    // console.log(req.body.account);
-    // res.json(req.body);
-    const sql = 'SELECT * FROM member WHERE email = ?';
+router.post("/login/api", async (req, res) => {
+  let output = {
+    success: false,
+    error: "",
+    member_sid: 0,
+    nickname: "",
+    token: "",
+  };
+  // console.log(req.body.account);
+  // res.json(req.body);
+  const sql = "SELECT * FROM member WHERE email = ?";
 
-    const [rows] = await db.query(sql, req.body.email);
+  const [rows] = await db.query(sql, req.body.email);
 
-    if(rows[0]?.password) {
-        const pwValidate = await bcrypt.compare(req.body.password, rows[0].password);
+  if (rows[0]?.password) {
+    const pwValidate = await bcrypt.compare(
+      req.body.password,
+      rows[0].password
+    );
 
-        if(pwValidate) {
-            const token = jwt.sign({member_sid: rows[0].sid}, process.env.JWT_SECRET)
-            output =  {
-                success: true,
-                member_sid: rows[0].sid,
-                nickname: rows[0].nickname,
-                token: token,
-            }
-        }
+    if (pwValidate) {
+      const token = jwt.sign(
+        { member_sid: rows[0].sid },
+        process.env.JWT_SECRET
+      );
+      output = {
+        success: true,
+        member_sid: rows[0].sid,
+        nickname: rows[0].nickname,
+        token: token,
+      };
     }
-    console.log({rows});
-    res.json(output);
-})
+  }
+  console.log(output);
+  res.json(output);
+});
 
-router.post('/register/api', async (req, res) => {
-    let output = {
-        success: false,
-        error: '',
+router.post("/register/api", async (req, res) => {
+  let output = {
+    success: false,
+    error: "",
+  };
+
+  if (!req.body.email || !req.body.password) {
+    output = {
+      ...output,
+      error: "參數不足",
     };
-
-    if(!req.body.email || !req.body.password) {
-        output = {
-            ...output,
-            error: '參數不足',
-        }
-    } else { 
-        const e_sql = `SELECT * FROM member WHERE email = ?`
-        const [e_rows] = await db.query(e_sql, req.body.email)
-        if (! e_rows[0]) {
-            const pw = await bcrypt.hash(req.body.password, 10);
-            const sql = `INSERT INTO member (email, password, nickname, birthday) VALUES (?, ?, ?, ?)`;
-            const [rows] = await db.query (sql, [
-                req.body.email,
-                pw,
-                req.body.nickname || '',
-                req.body.birthday || null,
-            ])
-            output = {
-                ...output,
-                success: true,
-                error: '',
-            }
-        } else {
-            output = {
-                ...output,
-                success: false,
-                error: '重複email',
-            }
-        }
-
+  } else {
+    const e_sql = `SELECT * FROM member WHERE email = ?`;
+    const [e_rows] = await db.query(e_sql, req.body.email);
+    if (!e_rows[0]) {
+      const pw = await bcrypt.hash(req.body.password, 10);
+      const sql = `INSERT INTO member (email, password, nickname, birthday) VALUES (?, ?, ?, ?)`;
+      const [rows] = await db.query(sql, [
+        req.body.email,
+        pw,
+        req.body.nickname || "",
+        req.body.birthday || null,
+      ]);
+      output = {
+        ...output,
+        success: true,
+        error: "",
+      };
+    } else {
+      output = {
+        ...output,
+        success: false,
+        error: "重複email",
+      };
     }
-    console.log(output);
-    res.json(output);
-})
-
+  }
+  console.log(output);
+  res.json(output);
+});
 
 module.exports = router;
