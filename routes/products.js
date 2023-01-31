89faa54de2fe0a36c94c5d5215a6e21359b9ced1
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-02-01 00:01:50 +0800

    add sql_count

--- routes/products.js
+++ routes/products.js
@@ -3,15 +3,32 @@ const router = express.Router();
 const db = require(__dirname + "/../modules/db_connect");
 
 router.get("/list/api", async (req, res) => {
-  const { prodCate } = req.query;
-  const where = +prodCate ? `&& pc.parent_sid =${prodCate}` : "";
+  const { prodCate, page } = req.query;
 
-  const sql = `SELECT p.* FROM products p JOIN product_categories pc ON p.category = pc.sid WHERE on_sale = 1 ${where}`;
-  console.log(sql);
+  // 分頁參數設定
+  const perPage = 20;
+  let nowPage = +page || 1;
+  let totalPages = 0;
 
-  const [rows] = await db.query(sql);
+  // 產品分類條件
+  let where = `WHERE on_sale = 1 `;
+  where += +prodCate ? `&& pc.parent_sid =${prodCate}` : "";
+
+  // 總共筆數&頁數
+  const sql_count = `SELECT COUNT(1) count FROM products p JOIN product_categories pc ON p.category = pc.sid ${where}`;
+  // const sql_count = `SELECT COUNT(1) count FROM products p JOIN product_categories pc ON p.category = pc.sid WHERE on_sale = 1 && pc.parent_sid = 1`;
+  const [[{ count }]] = await db.query(sql_count);
+  if (count > 0) totalPages = Math.ceil(count / perPage);
+  // console.log(count);
+
+  // 每頁內容
+  const sql_data = `SELECT p.* FROM products p JOIN product_categories pc ON p.category = pc.sid WHERE on_sale = 1 ${where} LIMIT ${
+    perPage * (nowPage - 1)
+  } ,${perPage}`;
+
+  const [rows] = await db.query(sql_data);
   // console.log({rows});
-  res.json(rows);
+  res.json({ rows, count, totalPages }); 
 });
 
 module.exports = router;
